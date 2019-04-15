db4af4d37e9cc066c8ab5e6f35878723468080a8
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-15 18:06:16 +0300

    formated code

--- panic/main_test.go
+++ panic/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	i := Sum(5,10)
-	if i!=15 {
-		t.Errorf("The sum wasnt correct, got %d, want %d",i,15)
+	i := Sum(5, 10)
+	if i != 15 {
+		t.Errorf("The sum wasnt correct, got %d, want %d", i, 15)
 	}
 }
--- sumerror/main_test.go
+++ sumerror/main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	i, _ := Sum("4","5")
-	if i!=9 {
-		t.Errorf("The sum wasnt correct, got %d, want %d",i,9)
+	i, _ := Sum("4", "5")
+	if i != 9 {
+		t.Errorf("The sum wasnt correct, got %d, want %d", i, 9)
 	}
 }
