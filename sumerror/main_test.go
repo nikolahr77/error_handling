408a2e2440213752d34e392410c603c8925e498a
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-15 18:11:16 +0300

    added a test for when error is thrown

--- sumerror/main_test.go
+++ sumerror/main_test.go
@@ -8,3 +8,10 @@ func TestSum(t *testing.T) {
 		t.Errorf("The sum wasnt correct, got %d, want %d", i, 9)
 	}
 }
+
+func TestSum_Error(t *testing.T) {
+	_, err := Sum("faagf", "5")
+	if err == nil {
+		t.Error("There should be an error when converting invalid string", err)
+	}
+}
